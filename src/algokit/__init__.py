f2595e28d4e921fcabd86daab9b3ec9b4a224ce9
Wilson Waters <wilson.waters@makerx.com.au>
2022-11-15 15:04:43 +0800

    Improved python version error messaging

--- src/algokit/__init__.py
+++ src/algokit/__init__.py
@@ -16,9 +16,8 @@ def cli():
 def check_python_version():
     installed_python_version = sys.version_info
     logging.debug(f'Python version {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} installed')
-    if installed_python_version < (3, 10):
-        raise ConfigurationError(f'AlgoKit CLI requires python v3.10.1 or higher to be installed. Your installed version is {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} Please visit https://www.python.org/downloads/ and install most recent version')
-
+    if installed_python_version > (3, 10):
+        raise ConfigurationError(f'Unsupported CPython version ({installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro}) detected. The minimum version of Python supported is CPython 3.10.\n\nIf you need help installing then this is a good starting point: https://www.python.org/about/gettingstarted/.')
 
 cli.add_command(init_command)
 cli.add_command(sandbox_group)
@@ -26,4 +25,9 @@ cli.add_command(version)
 
 
 if __name__ == "__main__":
-    cli()
+    try:
+        cli()
+    except ConfigurationError as ce:
+        logging.error(f'Configuration error: {str(ce)}')
+        sys.exit(1)
+
