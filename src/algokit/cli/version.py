58975c373c97783fc21c199a663326b275dfce46
Adam Chidlow <achidlow@users.noreply.github.com>
2022-11-15 16:44:34 +0800

    fix code style

--- src/algokit/cli/version.py
+++ src/algokit/cli/version.py
@@ -1,6 +1,7 @@
 import click
 from importlib import metadata
 
+
 @click.command("version", short_help="Show current version of AlgoKit cli")
 def version():
-    print(metadata.version('algokit'))
+    print(metadata.version("algokit"))
