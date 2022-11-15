e50a8b53d09a90e2061cb891b5eb4cb8aa923bc0
Wilson Waters <wilson.waters@makerx.com.au>
2022-11-15 15:27:42 +0800

    Improved console message with no commands

--- src/algokit/__init__.py
+++ src/algokit/__init__.py
@@ -9,14 +9,14 @@ from algokit.cli.version import version
 class ConfigurationError(Exception):
     pass
 
-@click.group()
+@click.group(help='AlgoKit is your one stop shop to develop applications on the Algorand blockchain.')
 def cli():
     check_python_version()
 
 def check_python_version():
     installed_python_version = sys.version_info
     logging.debug(f'Python version {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} installed')
-    if installed_python_version > (3, 10):
+    if installed_python_version < (3, 10):
         raise ConfigurationError(f'Unsupported CPython version ({installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro}) detected. The minimum version of Python supported is CPython 3.10.\n\nIf you need help installing then this is a good starting point: https://www.python.org/about/gettingstarted/.')
 
 cli.add_command(init_command)
