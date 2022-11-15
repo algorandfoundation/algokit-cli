6ca7058e70ed659449ef9c68ae509579cb947e19
Wilson Waters <wilson.waters@makerx.com.au>
2022-11-15 22:17:48 +0800

    run through code formatter

--- src/algokit/__init__.py
+++ src/algokit/__init__.py
@@ -6,12 +6,19 @@ from algokit.cli.init import init_command
 from algokit.cli.sandbox import sandbox_group
 from algokit.cli.version import version_command
 
+
 class ConfigurationError(Exception):
     pass
 
-CLICK_CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.group(help='AlgoKit is your one-stop shop to develop applications on the Algorand blockchain.', invoke_without_command=True, context_settings=CLICK_CONTEXT_SETTINGS)
+CLICK_CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.group(
+    help="AlgoKit is your one-stop shop to develop applications on the Algorand blockchain.",
+    invoke_without_command=True,
+    context_settings=CLICK_CONTEXT_SETTINGS,
+)
 @click.option("--version", help="Show current version of AlgoKit cli", is_flag=True)
 @click.pass_context
 def cli(ctx, version):
@@ -22,15 +29,20 @@ def cli(ctx, version):
 
     # The help output would normally show when no subcommands have been supplied,
     # but we override the default behavior through the invoke_without_command so we can show 'algokit --version'
-    if ctx.invoked_subcommand is None: 
+    if ctx.invoked_subcommand is None:
         print(ctx.get_help())
 
 
 def check_python_version():
     installed_python_version = sys.version_info
-    logging.debug(f'Python version {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} installed')
+    logging.debug(
+        f"Python version {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} installed"
+    )
     if installed_python_version < (3, 10):
-        raise ConfigurationError(f'Unsupported CPython version ({installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro}) detected. The minimum version of Python supported is CPython 3.10.\n\nIf you need help installing then this is a good starting point: https://www.python.org/about/gettingstarted/.')
+        raise ConfigurationError(
+            f"Unsupported CPython version ({installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro}) detected. The minimum version of Python supported is CPython 3.10.\n\nIf you need help installing then this is a good starting point: https://www.python.org/about/gettingstarted/."
+        )
+
 
 cli.add_command(init_command)
 cli.add_command(sandbox_group)
@@ -41,6 +53,5 @@ if __name__ == "__main__":
     try:
         cli()
     except ConfigurationError as ce:
-        logging.error(f'Configuration error: {str(ce)}')
+        logging.error(f"Configuration error: {str(ce)}")
         sys.exit(1)
-
--- src/algokit/cli/version.py
+++ src/algokit/cli/version.py
@@ -1,6 +1,7 @@
 import click
 from importlib import metadata
 
+
 @click.command("version", short_help="Show current version of AlgoKit cli")
 def version_command():
-    print(metadata.version('algokit'))
+    print(metadata.version("algokit"))
