9134ca00116239ada697db25ac22afaf8b1f846f
Wilson Waters <wilson.waters@makerx.com.au>
2022-11-15 14:56:13 +0800

    Python minimum version check

--- src/algokit/__init__.py
+++ src/algokit/__init__.py
@@ -1,13 +1,23 @@
 import click
+import sys
+import logging
 
 from algokit.cli.init import init_command
 from algokit.cli.sandbox import sandbox_group
 from algokit.cli.version import version
 
+class ConfigurationError(Exception):
+    pass
 
 @click.group()
 def cli():
-    pass
+    check_python_version()
+
+def check_python_version():
+    installed_python_version = sys.version_info
+    logging.debug(f'Python version {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} installed')
+    if installed_python_version < (3, 10):
+        raise ConfigurationError(f'AlgoKit CLI requires python v3.10.1 or higher to be installed. Your installed version is {installed_python_version.major}.{installed_python_version.minor}.{installed_python_version.micro} Please visit https://www.python.org/downloads/ and install most recent version')
 
 
 cli.add_command(init_command)
