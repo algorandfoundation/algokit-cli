e30ca2118028d05c266c46c666464732bf9002fa
Adam Chidlow <achidlow@users.noreply.github.com>
2022-11-17 13:50:08 +0800

    use builtin --version functionality

--- src/algokit/cli/version.py
+++
@@ -1,8 +0,0 @@
-from importlib import metadata
-
-import click
-
-
-@click.command("version", short_help="Show current version of AlgoKit cli")
-def version_command():
-    click.echo(metadata.version("algokit"))
