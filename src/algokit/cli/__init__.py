444e081094eaece19ba226e91be6f876b032d756
Adam Chidlow <achidlow@users.noreply.github.com>
2022-11-17 18:23:06 +0800

    configure logging & app dirs
    start with some basic cross-platform defaults for state and config locations
    
    configure logging so (by default), anything info or above gets printed by default, but DEBUG can be printed with -v/--verbose flag
    
    output all log statements to log files in app state dir, and rotate log files. max of 1MB per file, max 5 files for now
    
    use colours/styling for terminal output, but allow override with --color/--no-color flags or NO_COLOR env var
    
    handle missing packages or misconfigured root(s) for users by detecting import errors

--- src/algokit/cli/__init__.py
+++ src/algokit/cli/__init__.py
@@ -1,5 +1,9 @@
+import logging
+
 import click
 
+from algokit.core.conf import PACKAGE_NAME
+from algokit.core.log_handlers import console_log_handler
 from algokit.cli.init import init_command
 from algokit.cli.sandbox import sandbox_group
 
@@ -8,9 +12,14 @@ from algokit.cli.sandbox import sandbox_group
     help="AlgoKit is your one-stop shop to develop applications on the Algorand blockchain.",
     context_settings={"help_option_names": ["-h", "--help"]},
 )
-@click.version_option(package_name="algokit")
-def algokit():
-    pass
+@click.version_option(package_name=PACKAGE_NAME)
+@click.option("--verbose", "-v", is_flag=True)
+@click.option("--color/--no-color", default=None)
+def algokit(verbose: bool, color: bool | None):
+    if verbose:
+        console_log_handler.setLevel(logging.DEBUG)
+    if color is not None:
+        console_log_handler.force_styles_to = color
 
 
 algokit.add_command(init_command)
--- src/algokit/cli/sandbox.py
+++ src/algokit/cli/sandbox.py
@@ -1,13 +1,17 @@
+import logging
+
 import click
+
+logger = logging.getLogger(__name__)
 
 
 @click.group("sandbox", short_help="Manage the Algorand sandbox")
 def sandbox_group():
-    print("Hello I'm the sandbox command group")
+    logger.debug("Hello I'm the sandbox command group")
 
 
 @sandbox_group.command("restart")
 def restart_sandbox():
-    print("Restarting the sandbox now...")
+    logger.info("Restarting the sandbox now...")
     # TODO: the thing
-    print("Done!")
+    logger.info("Done!")
