3b2c526b75288dac2e55b6ceeaa757976f57e6d9
Rob Moore <rob.moore@makerx.com.au>
2022-11-18 13:32:30 +0800

    Renaming test method

--- tests/test_sandbox_start.py
+++ tests/test_sandbox_start.py
@@ -9,7 +9,7 @@ from pytest_mock import MockerFixture
 from algokit.cli import algokit
 
 
-def verify_output(stdout: str, additional_name: str | None = None, additional_output: str | None = None):
+def get_verify_output(stdout: str, additional_name: str | None = None, additional_output: str | None = None):
     return (
         f"""{stdout}----
 {additional_name}:
@@ -41,7 +41,7 @@ def test_sandbox_start(mocker: MockerFixture, tmp_path: Path):
 
     assert result.exit_code == 0
     verify(
-        verify_output(
+        get_verify_output(
             result.stdout.replace(str(tmp_path), "{app_config}"),
             "{app_config}/docker-compose.yml",
             (tmp_path / "docker-compose.yml").read_text(),
@@ -56,7 +56,7 @@ def test_sandbox_start_without_docker(mocker: MockerFixture, tmp_path: Path):
     result = invoke("sandbox start")
 
     assert result.exit_code == 1
-    verify(verify_output(result.stdout, "STDERR", result.stderr))
+    verify(get_verify_output(result.stdout, "STDERR", result.stderr))
 
 
 def test_sandbox_start_without_docker_compose(mocker: MockerFixture, tmp_path: Path):
@@ -66,7 +66,7 @@ def test_sandbox_start_without_docker_compose(mocker: MockerFixture, tmp_path: P
     result = invoke("sandbox start")
 
     assert result.exit_code == 1
-    verify(verify_output(result.stdout, "STDERR", result.stderr))
+    verify(get_verify_output(result.stdout, "STDERR", result.stderr))
 
 
 def test_sandbox_start_without_docker_engine_running(mocker: MockerFixture, tmp_path: Path):
@@ -76,4 +76,4 @@ def test_sandbox_start_without_docker_engine_running(mocker: MockerFixture, tmp_
     result = invoke("sandbox start")
 
     assert result.exit_code == 1
-    verify(verify_output(result.stdout, "STDERR", result.stderr))
+    verify(get_verify_output(result.stdout, "STDERR", result.stderr))
