6892ade609c35670fa2f62e240c27b8456067d0f
Adam Chidlow <achidlow@users.noreply.github.com>
2022-12-05 13:57:24 +0800

    feat(init): test output - supress logging of packages used by copier, and strip ascii control characters from the output, so we can use approval test here too

--- tests/init/test_init.py
+++ tests/init/test_init.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+import logging
 
 from _pytest.tmpdir import TempPathFactory
+from approvaltests import verify
 from prompt_toolkit.input import PipeInput
 from utils.click_invoker import invoke
+from click import unstyle
 
 PARENT_DIRECTORY = Path(__file__).parent
 GIT_BUNDLE_PATH = PARENT_DIRECTORY / "copier-script-v0.1.0.gitbundle"
@@ -13,6 +16,8 @@ def test_init_minimal_interaction_required_no_git_no_network(
 ):
     cwd = tmp_path_factory.mktemp("cwd")
 
+    logging.getLogger("plumbum.local").setLevel("INFO")
+    logging.getLogger("asyncio").setLevel("INFO")
     mock_questionary_input.send_text("Y")
     result = invoke(
         f"init --name myapp --no-git --template-url '{GIT_BUNDLE_PATH}' --answer script script.sh --answer nix yes",
@@ -22,3 +27,4 @@ def test_init_minimal_interaction_required_no_git_no_network(
     assert result.exit_code == 0
     paths = {p.relative_to(cwd) for p in cwd.rglob("*")}
     assert paths == {Path("myapp"), Path("myapp") / "script.sh"}
+    verify(unstyle(result.output))
