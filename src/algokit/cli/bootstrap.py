b6375f3fcedc0783e66637c345eb1415c212f48a
Rob Moore <rob.moore@makerx.com.au>
2022-12-13 23:49:29 +0800

    feat(bootstrap): Added algokit bootstrap all command and algokit bootstrap env command

--- src/algokit/cli/bootstrap.py
+++ src/algokit/cli/bootstrap.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import shutil
 import sys
 from pathlib import Path
 from platform import system
@@ -9,9 +11,70 @@ from algokit.core import proc
 logger = logging.getLogger(__name__)
 
 
+def bootstrap_cwd(context: click.Context) -> None:
+    current_dir = Path.cwd()
+    env_path = current_dir / ".env.template"
+    poetry_path = current_dir / "poetry.toml"
+    pyproject_path = current_dir / "pyproject.toml"
+
+    logger.debug(f"Checking {current_dir} for bootstrapping needs")
+
+    if env_path.exists():
+        logger.debug("Running `algokit bootstrap env`")
+        context.invoke(
+            env,
+        )
+
+    if poetry_path.exists() or (pyproject_path.exists() and "[tool.poetry]" in pyproject_path.read_text("utf-8")):
+        logger.debug("Running `algokit bootstrap poetry`")
+        context.invoke(poetry)
+
+
 @click.group("bootstrap", short_help="Bootstrap AlgoKit project dependencies.")
 def bootstrap_group() -> None:
     pass
+
+
+@bootstrap_group.command(
+    "all", short_help="Bootstrap all aspects of the current directory and immediate sub directories by convention."
+)
+@click.pass_context
+def bootstrap_all(context: click.Context) -> None:
+    base_path = Path.cwd()
+    bootstrap_cwd(context)
+
+    sub_dirs = base_path.glob("*/")
+    for sub_dir in sub_dirs:
+        if not sub_dir.is_dir():
+            continue
+        if sub_dir.name.lower() in [".venv", "node_modules", "__pycache__"]:
+            logger.debug(f"Skipping {sub_dir}")
+            continue
+        os.chdir(sub_dir)
+        try:
+            bootstrap_cwd(context)
+        finally:
+            os.chdir(base_path)
+
+    logger.info(f"Finished bootstrapping {base_path}")
+
+
+@bootstrap_group.command("env", short_help="Bootstrap .env file in the current working directory.")
+def env() -> None:
+    base_path = Path.cwd()
+    env_path = base_path / ".env"
+    env_template_path = base_path / ".env.template"
+
+    if env_path.exists():
+        logger.info(".env already exists; skipping bootstrap of .env")
+    else:
+        logger.debug(f"{env_path} doesn't exist yet")
+        if not env_template_path.exists():
+            logger.info("No .env or .env.template file; nothing to do here, skipping bootstrap of .env")
+        else:
+            logger.debug(f"{env_template_path} exists")
+            logger.info(f"Copying {env_template_path} to {env_path}")
+            shutil.copyfile(env_template_path, env_path)
 
 
 @bootstrap_group.command("poetry", short_help="Bootstrap Python Poetry and install in the current working directory.")
@@ -73,7 +136,7 @@ def poetry(*, ok_exit_code: bool) -> None:
             except IOError as e:
                 raise click.ClickException(
                     "Unable to access Poetry on PATH after installing it via pipx;"
-                    + " check pipx installations are on your path by running pipx ensurepath"
+                    + " check pipx installations are on your path by running `pipx ensurepath`"
                     + " and try `algokit bootstrap poetry` again."
                 ) from e
 
---
+++ tests/bootstrap/test_bootstrap_all.py
@@ -0,0 +1,94 @@
+from _pytest.tmpdir import TempPathFactory
+from utils.approvals import verify
+from utils.click_invoker import invoke
+from utils.proc_mock import ProcMock
+
+
+def test_bootstrap_all_empty(tmp_path_factory: TempPathFactory):
+    cwd = tmp_path_factory.mktemp("cwd")
+
+    result = invoke(
+        "bootstrap all",
+        cwd=cwd,
+    )
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_bootstrap_all_env(tmp_path_factory: TempPathFactory):
+    cwd = tmp_path_factory.mktemp("cwd")
+    (cwd / ".env.template").touch()
+
+    result = invoke(
+        "bootstrap all",
+        cwd=cwd,
+    )
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_bootstrap_all_poetry(tmp_path_factory: TempPathFactory, proc_mock: ProcMock):
+    cwd = tmp_path_factory.mktemp("cwd")
+    (cwd / "poetry.toml").touch()
+
+    result = invoke(
+        "bootstrap all",
+        cwd=cwd,
+    )
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_bootstrap_all_poetry_via_pyproject(tmp_path_factory: TempPathFactory, proc_mock: ProcMock):
+    cwd = tmp_path_factory.mktemp("cwd")
+    (cwd / "pyproject.toml").write_text("[tool.poetry]", encoding="utf-8")
+
+    result = invoke(
+        "bootstrap all",
+        cwd=cwd,
+    )
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_bootstrap_all_skip_dirs(tmp_path_factory: TempPathFactory):
+    cwd = tmp_path_factory.mktemp("cwd")
+    (cwd / ".venv").mkdir()
+    (cwd / "__pycache__").mkdir()
+    (cwd / "node_modules").mkdir()
+    (cwd / "file.txt").touch()
+    (cwd / "empty_dir").mkdir()
+    (cwd / "boring_dir").mkdir()
+    (cwd / "boring_dir" / "file.txt").touch()
+    (cwd / "double_nested_dir").mkdir()
+    (cwd / "double_nested_dir" / "nest1").mkdir()
+    (cwd / "double_nested_dir" / "nest2").mkdir()
+    (cwd / "double_nested_dir" / "nest2" / "file.txt").touch()
+
+    result = invoke(
+        "bootstrap all",
+        cwd=cwd,
+    )
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_bootstrap_all_sub_dir(tmp_path_factory: TempPathFactory, proc_mock: ProcMock):
+    cwd = tmp_path_factory.mktemp("cwd")
+    (cwd / "empty_dir").mkdir()
+    (cwd / "live_dir").mkdir()
+    (cwd / "live_dir" / ".env.template").touch()
+    (cwd / "live_dir" / "poetry.toml").touch()
+
+    result = invoke(
+        "bootstrap all",
+        cwd=cwd,
+    )
+
+    assert result.exit_code == 0
+    verify(result.output)
