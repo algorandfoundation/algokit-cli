e92e21c9c372c4148c7cd782fe18c1589818f8ff
Wilson Waters <wilson.waters@makerx.com.au>
2022-12-13 23:16:57 +0800

    feat(chocolatey): initial working version

---
+++ scripts/chocolatey/algokit/tools/chocolateyuninstall.ps1
@@ -0,0 +1,16 @@
+﻿# ensure pipx is installed. Just in case someone has removed it manually
+python -m pip --disable-pip-version-check install --user pipx
+if ($LASTEXITCODE -ne 0) {
+  Throw "Error configuring pipx for uninstalling"
+}
+
+# zap it
+pipx uninstall $env:ChocolateyPackageName | Tee-Object -Variable cmdOutput
+if ($LASTEXITCODE -ne 0) {
+  if ($cmdOutput -match "Nothing to uninstall" ) {
+    Write-Output "$($env:ChocolateyPackageName) already uninstalled by pipx. Ignoring"
+  }
+  else {
+    Throw "Error running pipx uninstall $($env:ChocolateyPackageName)"
+  }
+}
