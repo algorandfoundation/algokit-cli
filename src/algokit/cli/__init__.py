86671d0b966be7416f1f36ebf434c86da8d3d193
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2022-12-22 09:59:53 +0800

    feat(version-check): add version check functionality

--- src/algokit/cli/__init__.py
+++ src/algokit/cli/__init__.py
@@ -7,15 +7,19 @@ from algokit.cli.init import init_command
 from algokit.cli.sandbox import sandbox_group
 from algokit.core.conf import PACKAGE_NAME
 from algokit.core.log_handlers import color_option, verbose_option
+from algokit.core.version_check import do_version_check, skip_version_check_option, version_check_option
 
 
 @click.group(
     help="AlgoKit is your one-stop shop to develop applications on the Algorand blockchain.",
     context_settings={"help_option_names": ["-h", "--help"]},
+    result_callback=do_version_check,
 )
 @click.version_option(package_name=PACKAGE_NAME)
 @verbose_option
 @color_option
+@version_check_option
+@skip_version_check_option
 def algokit() -> None:
     pass
 
