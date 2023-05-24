1f06533aa0332ef6545813b724a36aac9c2789cc
Negar Abbasi <negar.abbasi@makerx.com.au>
2023-05-24 12:14:42 +1000

    test: Adding fixture to tests

--- tests/generate/test_generate_client.py
+++ tests/generate/test_generate_client.py
@@ -1,6 +1,7 @@
 import pathlib
 import shutil
 
+import pytest
 from _pytest.tmpdir import TempPathFactory
 
 from tests.utils.approvals import verify
@@ -14,35 +15,34 @@ def test_generate_help() -> None:
     verify(result.output)
 
 
-def test_generate_client_python(tmp_path_factory: TempPathFactory) -> None:
+@pytest.fixture()
+def application_json(tmp_path_factory: TempPathFactory) -> pathlib.Path:
     cwd = tmp_path_factory.mktemp("cwd")
     json_file = pathlib.Path(__file__).parent / "application.json"
     shutil.copy(json_file, cwd / "application.json")
-    result = invoke("generate client -a application.json -o client.py", cwd=cwd)
+    return cwd / "application.json"
+
+
+def test_generate_client_python(application_json: pathlib.Path) -> None:
+    result = invoke(f"generate client -a {application_json} -o client.py")
 
     assert result.exit_code == 0
     verify(result.output)
-    assert (cwd / "client.py").exists()
-    assert (cwd / "client.py").read_text()
+    assert (application_json.parent / "client.py").exists()
+    assert (application_json.parent / "client.py").read_text()
 
 
-def test_generate_client_typescript(tmp_path_factory: TempPathFactory) -> None:
-    cwd = tmp_path_factory.mktemp("cwd")
-    json_file = pathlib.Path(__file__).parent / "application.json"
-    shutil.copy(json_file, cwd / "application.json")
-    result = invoke("generate client -a application.json -o client.ts", cwd=cwd)
+def test_generate_client_typescript(application_json: pathlib.Path) -> None:
+    result = invoke(f"generate client -a {application_json} -o client.ts")
 
     assert result.exit_code == 0
     verify(result.output)
-    assert (cwd / "client.ts").exists()
-    assert (cwd / "client.ts").read_text()
+    assert (application_json.parent / "client.ts").exists()
+    assert (application_json.parent / "client.ts").read_text()
 
 
-def test_npm_installation_error(tmp_path_factory: TempPathFactory) -> None:
-    cwd = tmp_path_factory.mktemp("cwd")
-    json_file = pathlib.Path(__file__).parent / "application.json"
-    shutil.copy(json_file, cwd / "application.json")
-    result = invoke("generate client -a application.json -o client.ts", cwd=cwd)
+def test_npm_installation_error(application_json: pathlib.Path) -> None:
+    result = invoke(f"generate client -a {application_json} -o client.ts")
 
     assert result.exit_code == 1
     verify(result.output)
