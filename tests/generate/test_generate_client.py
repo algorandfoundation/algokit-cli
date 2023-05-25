fd24b95be11e4a77c7c419fabd0701214f92cf7c
Negar Abbasi <negar.abbasi@makerx.com.au>
2023-05-25 12:45:34 +1000

    test: Adding test generate client with tokens.

--- tests/generate/test_generate_client.py
+++ tests/generate/test_generate_client.py
@@ -4,6 +4,7 @@ import shutil
 import pytest
 from _pytest.tmpdir import TempPathFactory
 from algokit.cli.generate import format_client_name, snake_case
+from approvaltests.namer import NamerFactory
 
 from tests.utils.approvals import verify
 from tests.utils.click_invoker import invoke
@@ -25,22 +26,41 @@ def test_generate_help() -> None:
     verify(result.output)
 
 
-def test_generate_client_python(application_json: pathlib.Path) -> None:
-    result = invoke(f"generate client -a {application_json.name} -o client.py", cwd=application_json.parent)
+@pytest.mark.parametrize(
+    ("output_path", "expected_output_path"),
+    [
+        ("client.py", "client.py"),
+        ("client_%name%.py", "client_hello_world_app.py"),
+    ],
+)
+def test_generate_client_python(
+    application_json: pathlib.Path, output_path: pathlib.Path, expected_output_path: pathlib.Path
+) -> None:
+    result = invoke(f"generate client -a {application_json.name} -o {output_path}", cwd=application_json.parent)
 
     assert result.exit_code == 0
-    verify(result.output)
-    assert (application_json.parent / "client.py").exists()
-    assert (application_json.parent / "client.py").read_text()
+    verify(result.output.replace("\\", "/"), options=NamerFactory.with_parameters(output_path))
+    assert (application_json.parent / expected_output_path).exists()
+    assert (application_json.parent / expected_output_path).read_text()
 
 
-def test_generate_client_typescript(application_json: pathlib.Path) -> None:
-    result = invoke(f"generate client -a {application_json.name} -o client.ts", cwd=application_json.parent)
-
+@pytest.mark.parametrize(
+    ("output_path", "expected_output_path"),
+    [
+        ("client.ts", "client.ts"),
+        ("client_%name%.ts", "client_hello_world_app.ts"),
+    ],
+)
+def test_generate_client_typescript(
+    application_json: pathlib.Path, output_path: pathlib.Path, expected_output_path: pathlib.Path
+) -> None:
+    result = invoke(f"generate client -a {application_json.name} -o {output_path}", cwd=application_json.parent)
     assert result.exit_code == 0
-    verify(result.output.replace("npx.cmd", "npx"))
-    assert (application_json.parent / "client.ts").exists()
-    assert (application_json.parent / "client.ts").read_text()
+    verify(
+        result.output.replace("npx.cmd", "npx").replace("\\", "/"), options=NamerFactory.with_parameters(output_path)
+    )
+    assert (application_json.parent / expected_output_path).exists()
+    assert (application_json.parent / expected_output_path).read_text()
 
 
 def test_npx_missing(proc_mock: ProcMock, application_json: pathlib.Path) -> None:
