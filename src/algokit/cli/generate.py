0a6996b94ca8c5228bc4cf0b6b6aebfd4125c60c
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-07-21 16:57:45 +0200

    feat: draft implementation for advanced generate command

--- src/algokit/cli/generate.py
+++ src/algokit/cli/generate.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import click
 
+from algokit.core.conf import get_algokit_config
 from algokit.core.typed_client_generation import ClientGenerator
 
 logger = logging.getLogger(__name__)
@@ -65,3 +66,53 @@ def generate_client(output_path_pattern: str | None, app_spec_path_or_dir: Path,
         output_path = generator.resolve_output_path(app_spec, output_path_pattern)
         if output_path is not None:
             generator.generate(app_spec, output_path)
+
+
+def init_generator(answers: dict, path: Path) -> None:
+    # copier is lazy imported for two reasons
+    # 1. it is slow to import on first execution after installing
+    # 2. the import fails if git is not installed (which we check above)
+    # TODO: copier is typed, need to figure out how to force mypy to accept that or submit a PR
+    #       to their repo to include py.typed file
+    from copier.main import Worker  # type: ignore[import]
+
+    cwd = Path.cwd()
+    with Worker(
+        src_path=str(path),
+        dst_path=cwd,
+        data=answers,
+        quiet=True,
+    ) as copier_worker:
+        logger.debug(f"Running generator in {copier_worker.src_path}")
+        copier_worker.run_copy()
+
+
+try:
+    cwd = Path.cwd()
+    config = get_algokit_config(cwd)
+
+    if config and "generators" in config:
+        for generator_data in config["generators"].values():
+            command = click.command(name=generator_data["name"], help=generator_data["description"])(init_generator)
+            command = click.option(
+                "answers",
+                "--answer",
+                "-a",
+                multiple=True,
+                help="Answers key/value pairs to pass to the template.",
+                nargs=2,
+                default=[],
+                metavar="<key> <value>",
+            )(command)
+            command = click.option(
+                "path",
+                "--path",
+                "-p",
+                help=f"Path to {generator_data['name']} generator. (Default: {generator_data['path']})",
+                type=click.Path(exists=True),
+                default=generator_data["path"],
+            )(command)
+
+            generate_group.add_command(command)
+except Exception as ex:
+    logger.error(f"Error: {ex}")
