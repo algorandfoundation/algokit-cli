ea6990b94854eef7d594c4683809f713d85175a8
Al <aorumbayev@pm.me>
2023-10-11 11:01:40 +0200

    feat: transaction signing task; polishing tasks (#324)
    * feat: transaction signing task; polishing tasks
    
    * docs: adding docstrings; refining task utils
    
    * test: adding snapshot tests
    
    * chore: regen docs
    
    * docs: adding docs

--- src/algokit/cli/task.py
+++ src/algokit/cli/task.py
@@ -2,6 +2,7 @@ import logging
 
 import click
 
+from algokit.cli.tasks.sign_transaction import sign
 from algokit.cli.tasks.transfer import transfer
 from algokit.cli.tasks.vanity_address import vanity_address
 from algokit.cli.tasks.wallet import wallet
@@ -17,3 +18,4 @@ def task_group() -> None:
 task_group.add_command(wallet)
 task_group.add_command(vanity_address)
 task_group.add_command(transfer)
+task_group.add_command(sign)
