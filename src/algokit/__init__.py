88363ddceacb82b964bfc2323c04e1d5c335bba1
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-10-12 17:09:46 +0200

    feat: ipfs upload

--- src/algokit/__init__.py
+++ src/algokit/__init__.py
@@ -1,16 +1,6 @@
-import platform
 import sys
 
 # this isn't beautiful, but to avoid confusing user errors we need this check before we start importing our own modules
-if sys.version_info < (3, 10, 0):
-    print(  # noqa: T201
-        f"Unsupported CPython version: {platform.python_version()} detected.\n"
-        "The minimum version of Python supported is CPython 3.10.\n"
-        "If you need help installing then this is a good starting point: \n"
-        "https://www.python.org/about/gettingstarted/",
-        file=sys.stderr,
-    )
-    sys.exit(-1)
 
 try:
     from algokit.core.log_handlers import initialise_logging, uncaught_exception_logging_handler
