8a69b3cc79cfe324675694f084ca953ce755649a
Al <aorumbayev@pm.me>
2023-10-23 15:44:01 +0200

    chore: upgrading dependencies on algokit-utils and client generator (#337)

--- src/algokit/core/typed_client_generation.py
+++ src/algokit/core/typed_client_generation.py
@@ -13,7 +13,7 @@ from algokit.core import proc
 
 logger = logging.getLogger(__name__)
 
-TYPESCRIPT_NPX_PACKAGE = "@algorandfoundation/algokit-client-generator@^2.2.2"
+TYPESCRIPT_NPX_PACKAGE = "@algorandfoundation/algokit-client-generator@^2.2.5"
 
 
 def _snake_case(s: str) -> str:
