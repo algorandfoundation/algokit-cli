07e6c376bc0df64d908abab0c4d6e83cc21bda0b
Negar Abbasi <negar.abbasi@makerx.com.au>
2023-11-21 14:47:30 +1000

    chore: ignore updating python in ruff

--- src/algokit/__init__.py
+++ src/algokit/__init__.py
@@ -2,10 +2,10 @@ import platform
 import sys
 
 # this isn't beautiful, but to avoid confusing user errors we need this check before we start importing our own modules
-if sys.version_info < (3, 11, 0):
+if sys.version_info < (3, 10, 0):  # noqa: UP036
     print(  # noqa: T201
         f"Unsupported CPython version: {platform.python_version()} detected.\n"
-        "The minimum version of Python supported is CPython 3.11.\n"
+        "The minimum version of Python supported is CPython 3.10.\n"
         "If you need help installing then this is a good starting point: \n"
         "https://www.python.org/about/gettingstarted/",
         file=sys.stderr,
