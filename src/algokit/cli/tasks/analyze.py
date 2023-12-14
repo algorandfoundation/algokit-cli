63c44e3de9724ba29ddd6de895651990df7e0721
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-12-14 19:45:32 +0100

    refactor: polishing the analyze command

--- src/algokit/cli/tasks/analyze.py
+++ src/algokit/cli/tasks/analyze.py
@@ -1,11 +1,52 @@
-import os
-import sys
+import json
+import logging
 from pathlib import Path
-from typing import Optional
 
 import click
 
-from algokit.core.proc import run
+from algokit.cli.common.utils import MutuallyExclusiveOption
+from algokit.core.tasks.analyze import (
+    TEALER_ARTIFACTS_ROOT,
+    TEALER_DOT_FILES_ROOT,
+    TEALER_REPORTS_ROOT,
+    generate_filename,
+    generate_table_rows,
+    generate_tealer_command,
+    handle_baseline_diff,
+    load_tealer_report,
+    run_tealer,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_artifact_folders(output_dir: Path | None) -> None:
+    if output_dir:
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    TEALER_REPORTS_ROOT.mkdir(parents=True, exist_ok=True)
+    TEALER_ARTIFACTS_ROOT.mkdir(parents=True, exist_ok=True)
+    TEALER_DOT_FILES_ROOT.mkdir(parents=True, exist_ok=True)
+
+
+def display_analysis_summary(analysis_results: dict):
+    impact_frequency = {}
+    for file_path, result_rows in analysis_results.items():
+        click.echo(f"\nFile: {file_path}\n")
+        for result in result_rows:
+            click.echo(
+                f"Detector: {result[0]}\n"
+                f"Impact: {result[1]}\n"
+                f"Details: {result[2]}\n"
+                f"Execution Paths (#Lines):\n{result[3]}\n"
+            )
+            impact_frequency[result[1]] = impact_frequency.get(result[1], 0) + 1
+    # print summary by impact label
+    click.echo("\nTotal issues:")
+    for impact, frequency in impact_frequency.items():
+        click.echo(f"{impact}: {frequency}")
+
+    click.echo(f"Finished analyzing {len(analysis_results)} files.")
 
 
 @click.command(
@@ -15,15 +56,27 @@ from algokit.core.proc import run
 @click.option(
     "--file",
     "-f",
+    "files",
     multiple=True,
-    type=click.Path(exists=True, dir_okay=False),
+    cls=MutuallyExclusiveOption,
+    not_required_if=["directory", "recursive"],
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
     help="Path to the TEAL file to analyze. Supports multiple files in a single run.",
 )
 @click.option(
     "--directory",
     "-d",
-    type=click.Path(exists=True, file_okay=False),
+    cls=MutuallyExclusiveOption,
+    not_required_if=["file"],
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
     help="Path to a directory containing the TEAL files. Recursively search for all TEAL files within.",
+)
+@click.option(
+    "--recursive",
+    is_flag=True,
+    cls=MutuallyExclusiveOption,
+    not_required_if=["file"],
+    help="Recursively search for all TEAL files within the provided directory.",
 )
 @click.option(
     "--force",
@@ -33,46 +86,111 @@ from algokit.core.proc import run
 @click.option(
     "--baseline",
     is_flag=True,
-    help="Persist a baseline file to the .algokit directory. Any failures that exist in this baseline file will be ignored on the next run.",
+    help=(
+        "Exit with a non-zero code if any diffs are identified between the current "
+        "and baseline reports. By default baseline reports are stored in the "
+        ".algokit/static-analysis/artifacts folder. Alternatively, you can specify a "
+        "custom path using the --output option to compare against."
+    ),
 )
 @click.option(
     "-o",
     "--output",
-    "output_file_path",
+    "output_path",
     required=False,
     default=None,
-    type=click.Path(dir_okay=False, file_okay=True, resolve_path=True, path_type=Path),
-    help="File path where to store the results of the static analysis.",
+    type=click.Path(dir_okay=True, file_okay=False, resolve_path=True, path_type=Path),
+    help="Directory path where to store the results of the static analysis.",
 )
-def analyze(
-    file: list[Path], directory: Optional[Path], force: bool, baseline: bool, output_file_path: Path | None
+@click.option(
+    "--exclude",
+    "detectors_to_exclude",
+    multiple=True,
+    default=[],
+    type=click.STRING,
+    help="Exclude specific vulnerabilities from the analysis. Supports multiple exclusions in a single run.",
+)
+@click.option(
+    "--diff",
+    "show_diff",
+    is_flag=True,
+    help="Show the diff between the current and baseline reports.",
+)
+def analyze(  # noqa: PLR0913
+    *,
+    files: list[Path],
+    directory: Path | None,
+    recursive: bool,
+    force: bool,
+    baseline: bool,
+    output_path: Path | None,
+    detectors_to_exclude: list[str],
+    show_diff: bool,
 ) -> None:
+    """
+    Analyze the TEAL programs for common vulnerabilities.
+
+    Args:
+        files (list[Path]): The files to analyze.
+        directory (Path | None): The directory containing the TEAL files.
+        recursive (bool): Whether to recursively search for all TEAL files within the provided directory.
+        force (bool): Whether to force verification without the disclaimer confirmation prompt.
+        baseline (bool): Whether to exit with a non-zero code if any diffs are
+        identified between the current and baseline reports.
+        output_path (Path | None): The directory path where to store the results of the static analysis.
+        detectors_to_exclude (list[str]): The vulnerabilities to exclude from the analysis.
+        show_diff (bool): Whether to show the diff between the current and baseline reports.
+    """
+
+    prepare_artifact_folders(output_path)
+
+    input_files = sorted(set(files))
+    detectors_to_exclude = sorted(set(detectors_to_exclude))
+
     if not force:
         click.confirm(
-            "Standard disclaimer: This tool provides suggestions for improving your TEAL programs, but it does not guarantee their correctness or security. Do you understand?",
+            "Standard disclaimer: This tool provides suggestions for improving "
+            "your TEAL programs, but it does not guarantee their correctness "
+            "or security. Do you understand?",
             abort=True,
         )
-    files = [Path(f) for f in file]
+
     if directory:
-        files.extend(directory.glob("**/*.teal"))
-    for cur_file in files:
-        command = ["poetry", "run", "tealer", "detect", "--contracts", str(cur_file)]
-        if output_file_path is not None:
-            command.insert(3, "--json")
-            command.insert(4, str(Path.resolve(output_file_path)))
+        pattern = "**/*.teal" if recursive else "*.teal"
+        input_files.extend(sorted(directory.glob(pattern)))
 
-        result = run(
-            command,
-            env={
-                "TEALER_ROOT_OUTPUT_DIR": "/Users/aorumbayev/MakerX/projects/algokit/algokit-cli/.algokit/static-analysis",
-                **os.environ,
-            },
-        )
+    duplicate_files = {}
+    reports = {}
+    for cur_file in input_files:
+        file = cur_file.resolve()
+        filename = generate_filename(file, duplicate_files)
 
-        if baseline:
-            with open(Path(".algokit/baseline"), "a") as f:
-                f.write(f"{cur_file}: {result.output}")
-        else:
-            click.echo("To ignore these failures in the future, run with --baseline.")
+        report_output_root = output_path or TEALER_REPORTS_ROOT
+        report_output_path = report_output_root / filename
 
-        sys.exit(result.exit_code)
+        command = generate_tealer_command(cur_file, report_output_path, detectors_to_exclude)
+
+        old_report = load_tealer_report(str(report_output_path)) if report_output_path.exists() and baseline else None
+
+        result = run_tealer(command)
+
+        if baseline and old_report:
+            handle_baseline_diff(
+                cur_file=cur_file, report_output_path=report_output_path, old_report=old_report, show_diff=show_diff
+            )
+
+        reports[str(report_output_path.absolute())] = json.load(report_output_path.open())
+
+        if result.exit_code != 0:
+            click.echo(
+                f"An error occurred while analyzing {cur_file}. Please check the logs for more information.",
+                err=True,
+            )
+            raise click.Abort("Error while running tealer")
+
+    table_rows = generate_table_rows(reports)
+
+    if table_rows:
+        display_analysis_summary(table_rows)
+    else:
+        click.echo("No issues identified.")
--- src/algokit/core/tasks/analyze.py
+++ src/algokit/core/tasks/analyze.py
@@ -1,6 +1,19 @@
-from typing import Optional
+import json
+import logging
+import os
+import re
+from pathlib import Path
 
+from jsondiff import diff
 from pydantic import BaseModel, Field
+
+from algokit.core.proc import RunResult, run
+
+logger = logging.getLogger(__name__)
+
+TEALER_REPORTS_ROOT = Path.cwd() / Path(".algokit/static-analysis")
+TEALER_ARTIFACTS_ROOT = TEALER_REPORTS_ROOT / "artifacts"
+TEALER_DOT_FILES_ROOT = TEALER_REPORTS_ROOT / "tealer"
 
 
 class TealerBlock(BaseModel):
@@ -21,10 +34,92 @@ class TealerExecutionPath(BaseModel):
 
 class TealerAnalysisReport(BaseModel):
     success: bool
-    data_error: Optional[str] = Field(alias="error")
+    data_error: str | None = Field(alias="error")
     result: list[TealerExecutionPath]
 
 
-# Usage
-# data = json.loads(json_string)
-# root = Root(**data)
+def load_tealer_report(file_path: str) -> TealerAnalysisReport:
+    with Path(file_path).open() as file:
+        data = json.load(file)
+    return TealerAnalysisReport(**data)
+
+
+def extract_line(block: list[str]) -> str:
+    return f"{int(block[0].split(':')[0])}-{int(block[-1].split(':')[0])}"
+
+
+def extract_lines(block: list[list[str]]) -> str:
+    return "->".join([extract_line(b) for b in block])
+
+
+def generate_filename(file: Path, duplicate_files: dict) -> str:
+    filename = f"{file.parent.stem}_{file.stem}"
+    if filename in duplicate_files:
+        duplicate_files[filename] += 1
+        filename = f"{filename}_{duplicate_files[filename]}.json"
+    else:
+        duplicate_files[filename] = 0
+        filename = f"{filename}.json"
+    return filename
+
+
+def generate_tealer_command(cur_file: Path, report_output_path: Path, detectors_to_exclude: list[str]) -> list[str]:
+    command = [
+        "poetry",
+        "run",
+        "tealer",
+        "--json",
+        str(report_output_path),
+        "detect",
+        "--contracts",
+        str(cur_file),
+    ]
+    if detectors_to_exclude:
+        for detector in detectors_to_exclude:
+            command.extend(["--exclude", detector])
+    return command
+
+
+def run_tealer(command: list[str]) -> RunResult:
+    return run(
+        command,
+        cwd=Path.cwd(),
+        env={
+            "TEALER_ROOT_OUTPUT_DIR": str(TEALER_DOT_FILES_ROOT),
+            **os.environ,
+        },
+    )
+
+
+def handle_baseline_diff(
+    *, cur_file: Path, report_output_path: Path, old_report: TealerAnalysisReport, show_diff: bool
+):
+    new_report = load_tealer_report(str(report_output_path))
+    baseline_diff = diff(old_report.model_dump(), new_report.model_dump())
+    if baseline_diff:
+        logger.info(f"Diff detected in {cur_file}! Please check the content of " f"{report_output_path}.")
+        if show_diff:
+            logger.error(baseline_diff)
+            logger.error("Diff detected in the report.")
+        else:
+            logger.info("To output the diff use --diff option.")
+
+
+def generate_table_rows(reports: dict) -> dict:
+    table_rows = {}
+    for report_path, _ in reports.items():
+        report = load_tealer_report(report_path)
+        file_path = Path(report_path).relative_to(Path.cwd())
+        for path in report.result:
+            if path.count == 0:
+                continue
+            check_type = path.check
+            impact = path.impact
+            description = path.description + " " + path.data_help
+            url = re.search(r"(?P<url>https?://[^\s]+)", description)
+            description_and_help_text = url.group("url") if url else description
+            paths = ",\n".join(extract_lines(block.blocks) for block in path.paths) or "N/A"
+            if file_path not in table_rows:
+                table_rows[file_path] = []
+            table_rows[file_path].append([check_type, impact, description_and_help_text, paths])
+    return table_rows
