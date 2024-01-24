cd575fdd4d881dd83043b323c7df075893794026
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-01-24 17:54:01 +1000

    feat: distributing native binary with package managers

---
+++ stores/check_version.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+SNAP_NAME="${APP_NAME}"
+
+sudo snap install --channel stable --classic snapcraft
+
+echo "Architecture: ${ARCHITECTURE}"
+
+SNAP_VERSION=$( snapcraft list-revisions "${SNAP_NAME}" | grep -F "stable*" | grep "${ARCHITECTURE}" | tr -s ' ' | cut -d ' ' -f 4 )
+echo "Snap version: ${SNAP_VERSION}"
+
+wget --quiet "https://api.github.com/repos/${ASSETS_REPOSITORY}/releases" -O gh_latest.json
+GH_VERSION=$( jq -r 'sort_by(.tag_name)|last.tag_name' gh_latest.json )
+echo "GH version: ${GH_VERSION}"
+
+rm -f gh_latest.json
+
+if [[ "${SNAP_VERSION}" == "${GH_VERSION}" ]]; then
+  export SHOULD_DEPLOY="no"
+else
+  export SHOULD_DEPLOY="yes"
+
+  snap version
+  snap info "${SNAP_NAME}" || true
+fi
+
+if [[ "${GITHUB_ENV}" ]]; then
+	echo "SHOULD_DEPLOY=${SHOULD_DEPLOY}" >> "${GITHUB_ENV}"
+fi
