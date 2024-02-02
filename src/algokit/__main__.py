89f23c89b4bb5187c552a17fcb57952540acafa0
Negar Abbasi <negar.abbasi@makerx.com.au>
2024-02-02 18:41:43 +1000

    fix: fixing vanity address and init command issues with pyinstaller

--- src/algokit/__main__.py
+++ src/algokit/__main__.py
@@ -1,3 +1,8 @@
+import sys
+from multiprocessing import freeze_support
+
 from algokit.cli import algokit
 
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    freeze_support()
 algokit()
--- src/algokit/cli/init.py
+++ src/algokit/cli/init.py
@@ -13,6 +13,7 @@ from algokit.core import proc, questionary_extensions
 from algokit.core.bootstrap import bootstrap_any_including_subdirs, project_minimum_algokit_version_check
 from algokit.core.log_handlers import EXTRA_EXCLUDE_FROM_CONSOLE
 from algokit.core.sandbox import DEFAULT_ALGOD_PORT, DEFAULT_ALGOD_SERVER, DEFAULT_ALGOD_TOKEN, DEFAULT_INDEXER_PORT
+from algokit.core.utils import get_python_paths
 
 logger = logging.getLogger(__name__)
 
@@ -215,6 +216,14 @@ def init_command(  # noqa: PLR0913
     directory_name = project_path.name
     # provide the directory name as an answer to the template, if not explicitly overridden by user
     answers_dict.setdefault("project_name", directory_name)
+
+    # This is temporary until we have a better way to handle this with copier
+    if "algorandfoundation/algokit-fullstack-template" in template.url:
+        system_python_path = next(get_python_paths(), None)
+        if system_python_path is not None:
+            answers_dict.setdefault("system_path", system_python_path)
+        else:
+            raise click.ClickException("You need to have python installed to use this command")
 
     logger.info("Starting template copy and render...")
     # copier is lazy imported for two reasons
