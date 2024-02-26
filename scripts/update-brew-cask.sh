a1150d639549acef376d6b385794f91847da8f7b
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-02-26 13:39:03 +1000

    chore: fix

--- scripts/update-brew-cask.sh
+++ scripts/update-brew-cask.sh
@@ -62,7 +62,7 @@ create_cask() {
     version_beta=${BASH_REMATCH[2]}
   fi
 
-  release_tag="${version}"
+  release_tag="v${version}"
   if [[ -n $version_beta ]]; then
     release_tag=${release_tag}-beta.${version_beta}
   fi
