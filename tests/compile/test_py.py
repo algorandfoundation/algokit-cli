01f0015e84ff6cc3a3749f3ccdb979ca7b3988c5
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-02-29 19:02:01 +1000

    chore: first test

---
+++ tests/compile/test_py.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import pytest
+from pytest_mock import MockerFixture
+
+from tests.utils.approvals import verify
+from tests.utils.click_invoker import invoke
+from tests.utils.proc_mock import ProcMock
+
+
+@pytest.fixture()
+def hello_world_contract_path() -> Path:
+    return Path(__file__).parent / "hello_world_contract.py"
+
+
+def test_compile_py_help() -> None:
+    result = invoke("compile py -h")
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_puyapy_not_installed_anywhere(hello_world_contract_path: Path, mocker: MockerFixture) -> None:
+    proc_mock = ProcMock()
+    proc_mock.should_bad_exit_on(["poetry", "run", "puyapy", "-h"], exit_code=1, output=["Puyapy not found"])
+    proc_mock.should_bad_exit_on(["puyapy", "-h"], exit_code=1, output=["Puyapy not found"])
+
+    proc_mock.set_output(["pipx", "--version"], ["1.0.0"])
+    proc_mock.set_output(["pipx", "install", "puya"], ["Puyapy is installed"])
+    proc_mock.set_output(["puyapy", str(hello_world_contract_path)], ["Done"])
+
+    mocker.patch("algokit.core.proc.Popen").side_effect = proc_mock.popen
+
+    result = invoke(f"compile py {hello_world_contract_path}")
+
+    assert result.exit_code == 0
+    verify(result.output)
