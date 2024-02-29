71c3bd0ef662efd99eb6467f6f244d611167730c
Patrick Dinh <patrick.dinh@makerx.com.au>
2024-03-01 08:59:31 +1000

    chore: tests

--- tests/compile/test_py.py
+++ tests/compile/test_py.py
@@ -24,13 +24,41 @@ def test_compile_py_help() -> None:
     verify(result.output)
 
 
-def test_puyapy_not_installed_anywhere(hello_world_contract_path: Path, mocker: MockerFixture) -> None:
+def test_puyapy_is_not_installed_anywhere(hello_world_contract_path: Path, mocker: MockerFixture) -> None:
     proc_mock = ProcMock()
     proc_mock.should_bad_exit_on(["poetry", "run", "puyapy", "-h"], exit_code=1, output=["Puyapy not found"])
     proc_mock.should_bad_exit_on(["puyapy", "-h"], exit_code=1, output=["Puyapy not found"])
 
     proc_mock.set_output(["pipx", "--version"], ["1.0.0"])
     proc_mock.set_output(["pipx", "install", "puya"], ["Puyapy is installed"])
+    proc_mock.set_output(["puyapy", str(hello_world_contract_path)], ["Done"])
+
+    mocker.patch("algokit.core.proc.Popen").side_effect = proc_mock.popen
+
+    result = invoke(f"compile py {_normalize_path(hello_world_contract_path)}")
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_puyapy_is_installed_in_project(hello_world_contract_path: Path, mocker: MockerFixture) -> None:
+    proc_mock = ProcMock()
+    proc_mock.set_output(["poetry", "run", "puyapy", "-h"], output=["Puyapy help"])
+    proc_mock.set_output(["poetry", "run", "puyapy", str(hello_world_contract_path)], ["Done"])
+
+    mocker.patch("algokit.core.proc.Popen").side_effect = proc_mock.popen
+
+    result = invoke(f"compile py {_normalize_path(hello_world_contract_path)}")
+
+    assert result.exit_code == 0
+    verify(result.output)
+
+
+def test_puyapy_is_installed_globally(hello_world_contract_path: Path, mocker: MockerFixture) -> None:
+    proc_mock = ProcMock()
+    proc_mock.should_bad_exit_on(["poetry", "run", "puyapy", "-h"], exit_code=1, output=["Puyapy not found"])
+
+    proc_mock.set_output(["puyapy", "-h"], output=["Puyapy help"])
     proc_mock.set_output(["puyapy", str(hello_world_contract_path)], ["Done"])
 
     mocker.patch("algokit.core.proc.Popen").side_effect = proc_mock.popen
