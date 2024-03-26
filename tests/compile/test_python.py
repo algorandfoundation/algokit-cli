9161325c64a4805689bb183e2b0330b9c1dfeee1
Neil Campbell <neil.campbell@makerx.com.au>
2024-03-27 00:05:16 +0800

    chore: fix windows compile test

--- tests/compile/test_python.py
+++ tests/compile/test_python.py
@@ -114,17 +114,17 @@ def test_puyapy_is_installed_globally(dummy_contract_path: Path, mocker: MockerF
     verify(result.output)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 12) or is_windows(), reason="PuyaPy requires python3.12 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 12), reason="PuyaPy requires python3.12 or higher")
 def test_valid_contract(cwd: Path, output_path: Path) -> None:
     subprocess.run([sys.executable, "-m", "venv", ".venv"], check=True, cwd=cwd)
     venv_path = cwd / ".venv"
-    venv_bin = f"{venv_path / 'Scripts'}" if is_windows() else f"{venv_path / 'bin'}"
+
     puyapy_env = {
         # Set NO_COLOR to 1 to avoid requirements for colorama on Windows
         "NO_COLOR": "1",
         "VIRTUAL_ENV": str(venv_path),
         "PYTHONHOME": "",
-        "PATH": f"{venv_bin}:{os.environ['PATH']}",
+        "PATH": (f"{venv_path / 'Scripts'};" if is_windows() else f"{venv_path / 'bin'}:" f"{os.environ['PATH']}"),
     }
 
     subprocess.run(["pip", "install", "algorand-python"], check=True, cwd=cwd, env=dict(os.environ) | puyapy_env)
