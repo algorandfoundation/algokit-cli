d4759d6258b89a31e3ec69541b46c4e2e6dcbdd6
Neil Campbell <neil.campbell@makerx.com.au>
2024-07-29 19:29:47 +0800

    chore:  native binary distribution via winget (#430)
    * chore: create the windows binary, installer and publish to winget

---
+++ scripts/winget/update-package.ps1
@@ -0,0 +1,23 @@
+Param(
+  [Parameter(Mandatory=$true)]
+  [String]
+  $releaseVersionTag
+)
+
+Function ThrowOnNonZeroExit {
+  Param( [String]$Message )
+  If ($LastExitCode -ne 0) {
+    Throw $Message
+  }
+}
+
+$wingetPackage = 'AlgorandFoundation.AlgoKit'
+$release = Invoke-RestMethod -uri "https://api.github.com/repos/algorandfoundation/algokit-cli/releases/tags/$releaseVersionTag"
+$installerUrl = $release | Select -ExpandProperty assets -First 1 | Where-Object -Property name -match '-windows_x64-winget\.msix$' | Select -ExpandProperty browser_download_url
+$releaseVersion = $releaseVersionTag.Trim("v")
+
+$wingetDir = New-Item -Force -ItemType Directory -Path .\build\winget
+$wingetExecutable = "$wingetDir\wingetcreate.exe"
+Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile $wingetExecutable
+& $wingetExecutable update $wingetPackage -s -v $releaseVersion -u "$installerUrl" -t "$env:WINGET_GITHUB_TOKEN"
+ThrowOnNonZeroExit "Failed to update winget package"
