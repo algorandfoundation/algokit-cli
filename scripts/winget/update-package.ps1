cc4baa3f9d842a576f315f76ac70ea3666d64ae1
Neil Campbell <neil.campbell@makerx.com.au>
2024-08-02 22:31:32 +0800

    chore: add winget cd support (#551)
    * chore: add winget cd support

--- scripts/winget/update-package.ps1
+++ scripts/winget/update-package.ps1
@@ -1,7 +1,7 @@
 Param(
-  [Parameter(Mandatory=$true)]
+  [Parameter(Mandatory = $true)]
   [String]
-  $releaseVersionTag
+  $releaseVersion
 )
 
 Function ThrowOnNonZeroExit {
@@ -12,9 +12,8 @@ Function ThrowOnNonZeroExit {
 }
 
 $wingetPackage = 'AlgorandFoundation.AlgoKit'
-$release = Invoke-RestMethod -uri "https://api.github.com/repos/algorandfoundation/algokit-cli/releases/tags/$releaseVersionTag"
+$release = Invoke-RestMethod -uri "https://api.github.com/repos/algorandfoundation/algokit-cli/releases/tags/v$releaseVersion"
 $installerUrl = $release | Select -ExpandProperty assets -First 1 | Where-Object -Property name -match '-windows_x64-winget\.msix$' | Select -ExpandProperty browser_download_url
-$releaseVersion = $releaseVersionTag.Trim("v")
 
 $wingetDir = New-Item -Force -ItemType Directory -Path .\build\winget
 $wingetExecutable = "$wingetDir\wingetcreate.exe"
