800e25e1910a58d969c38cf8063b84e49a892e8c
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2024-08-23 18:35:50 +0200

    chore: testing ci

--- tests/portability/test_pyinstaller_binary.py
+++ tests/portability/test_pyinstaller_binary.py
@@ -22,7 +22,7 @@ def command_str_to_list(command: str) -> list[str]:
         (command_str_to_list("algokit --help"), [0]),
         (command_str_to_list("algokit doctor"), [0]),
         (command_str_to_list("algokit task vanity-address PY"), [0]),
-        (command_str_to_list("algokit -v init --name playground -t python --no-git --no-ide"), [0]),
+        (command_str_to_list("algokit -v init --name playground -t python --no-git --no-ide --defaults"), [0]),
         (command_str_to_list("algokit project run build -- hello_world"), [0]),
     ],
 )
