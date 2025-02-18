745ec2cf9b13be7c32776048ceacbf071b9970ba
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2025-02-18 14:06:59 +0100

    chore: tmp disable audit

--- tests/portability/test_pyinstaller_binary.py
+++ tests/portability/test_pyinstaller_binary.py
@@ -51,7 +51,7 @@ def test_algokit_init_and_project_run(tmp_path_factory: pytest.TempPathFactory) 
     init_command = command_str_to_list(
         "algokit init --name playground "
         "--template-url=https://github.com/algorandfoundation/algokit-python-template "
-        "--template-url-ref=feat/algokit_utils_v3 --no-git --no-ide --defaults"
+        "--template-url-ref=feat/algokit_utils_v3 --no-git --no-ide --defaults --UNSAFE-SECURITY-accept-template-url"
     )
     init_result = subprocess.run(init_command, capture_output=True, text=True, check=False, cwd=cwd)
     logger.info(f"Command {init_command} returned {init_result.stdout}")
